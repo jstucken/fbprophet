b3ca7afbf9c3a6de2733d80d0b1879035ac6aff8
Jonathan Stucken <jonathan.stucken@swsphn.com.au>
2023-09-08 11:36:42 +1000

    Improve df creation

--- fbprophet.py
+++ fbprophet.py
@@ -1,10 +1,12 @@
 # This will open a new tab in your default web browser and display the Plotly graph
 
-from prophet import Prophet
-import yfinance as yf
+from datetime import datetime, timedelta
+
 import pandas as pd
 import plotly.io as pio
-from datetime import datetime, timedelta
+import yfinance as yf
+from prophet import Prophet
+from prophet.plot import plot_plotly
 
 symbol = input("Enter stock symbol e.g. AAPL, or GEAR.AX for aussie stocks: ")
 
@@ -16,12 +18,16 @@ start_date = end_date - timedelta(days=1095)
 data = yf.download(symbol, start_date, end_date)
 data.sort_values(by=["Date"], inplace=True)
 
-# save a csv
+# dump data to csv
 data.to_csv("data.csv")
 
-df = pd.read_csv("data.csv")
+# copy the yahoo dataframe for modification
+df = data.copy()
 
-# select the date and the price
+# reset the index of the df dataframe
+df.reset_index(inplace=True)
+
+# select just the date and the price
 df = df[["Date", "Close"]]
 
 # rename the features
@@ -32,10 +38,7 @@ fbp.fit(df)
 future = fbp.make_future_dataframe(periods=365)
 forecast = fbp.predict(future)
 
-# import the facebooks prophet plotting libraries
-from prophet.plot import plot_plotly
-
-# set the default renderer to 'browser'
+# set the default plot renderer to 'browser'
 pio.renderers.default = "browser"
 
 # display the figure using plotly.io.show()
